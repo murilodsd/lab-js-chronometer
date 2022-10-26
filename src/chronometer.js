35fc386489b4ae3103cf94e1706ac06a6c099a06
murilodsd <mdsouzadias@hotmail.com>
2022-10-25 22:11:32 -0300

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,47 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    // cuidado com o this dentro do setInterval e setTimeOut,
+    // e apontar para o objeto global para isso nao acontecer
+    // usamos arrow functoion
+    this.intervalId = setInterval(() => {
+      this.currentTime = this.currentTime + 1;
+      if (printTimeCallback) printTimeCallback();
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    //Para usar toString() direto em um number, precisamos por o numero entre parenteses
+    if (value.toString().length < 2) {
+      return ("0" + value.toString()).slice(-2);
+    } else return value.toString();
+
+    //return value.toString().padStart(2,'0')
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes(this.currentTime));
+    let seconds = this.computeTwoDigitNumber(this.getSeconds(this.currentTime));
+    return minutes + ':' + seconds
   }
 }
